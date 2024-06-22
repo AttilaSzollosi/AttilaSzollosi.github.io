bef295a3117f2890d9f6dad70b650f0b78112f56
Attila <attilas.szollosis@gmail.com>
2024-06-22 21:39:06 +0200

    Deployed

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VWKKKWEP.mjs";import"./chunk-ZM63JT7Q.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AGQWR37O.mjs";import"./chunk-H47VZKTK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
