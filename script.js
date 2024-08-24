0b293b801079d17dcf83f97ee84d60732caf74d9
Attila <attilas.szollosis@gmail.com>
2024-08-24 21:24:34 +0200

    Add skill descriptions and formatting changes

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 function setLanguage(language) {
-    const elements = document.querySelectorAll('[data-lang]');
-    elements.forEach(element => {
-        if (element.getAttribute('data-lang') === language) {
-            element.style.display = '';
-        } else {
-            element.style.display = 'none';
-        }
-    });
+  const elements = document.querySelectorAll("[data-lang]");
+  elements.forEach((element) => {
+    if (element.getAttribute("data-lang") === language) {
+      element.style.display = "";
+    } else {
+      element.style.display = "none";
+    }
+  });
 }
 
 // Add this script for mobile menu toggle
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navbar = document.querySelector('.navbar');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navbar = document.querySelector(".navbar");
 
-    menuToggle.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-    });
+  menuToggle.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Smooth scroll function
-    function smoothScroll(target) {
-        document.querySelector(target).scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
-
-    // Add click event listeners to sidebar links
-    document.querySelectorAll('.sidebar a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            smoothScroll(targetId);
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  // Smooth scroll function
+  function smoothScroll(target) {
+    document.querySelector(target).scrollIntoView({
+      behavior: "smooth",
     });
+  }
+
+  // Add click event listeners to sidebar links
+  document.querySelectorAll(".sidebar a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      smoothScroll(targetId);
+    });
+  });
 });
 
-document.querySelectorAll('.project').forEach(project => {
-    project.addEventListener('click', function() {
-        this.classList.toggle('open');
-    });
+document.querySelectorAll(".project").forEach((project) => {
+  project.addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
 });
